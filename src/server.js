583cdbd6804ef8592a300555c8e9d2c9b6ce9e3c
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-25 19:07:34 +0700

    Make HTTP Request & Save Note

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('Hallo kita akan membuat RESTful API');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
